fb219a02010b37180b136b02a07d2042f32e6df1
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-13 12:27:35 +0300

    first push

---
+++ src/Main.kt
@@ -0,0 +1,34 @@
+import kotlin.math.hypot
+
+fun main() {
+var number = 22
+
+
+    val maxIntgerValue = Int.MAX_VALUE
+    val minIntgervalue= Int.MIN_VALUE
+
+    println("Intger maximum value is : $maxIntgerValue")
+    println("Minimum value is : $minIntgervalue")
+
+    number =2147483647
+
+    val myMaxByteValu: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+
+    println("Byte maximum value is : $myMaxByteValu")
+    println("Byte Minimum value is : $myMinByteValue")
+
+    val myMaxShortValu: Short = Short.MAX_VALUE
+    val myMinShortValue: Short = Short.MIN_VALUE
+    println("Short maximum value is : $myMaxShortValu")
+    println("Short Minimum value is : $myMinShortValue")
+
+    val myMaxLongValue: Long = Long.MAX_VALUE
+    val myMinLongValue: Long = Long.MIN_VALUE
+
+    println("Long maximum value is : $myMaxLongValue")
+    println("Long Minimum value is : $myMinLongValue")
+
+    val myNumber = 288888888888888L
+
+}
