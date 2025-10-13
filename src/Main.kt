13fd2d32e134aead1b01b6269b47cc9e642402b6
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-13 17:05:57 +0300

    project3

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,44 @@
 import kotlin.math.hypot
 fun main() {
-    val myCharValue: Char = 'D'
-    val myBooleanValuealue: Boolean = false
+  var x = 5
+  val y = 3
 
 
+    println("x + y = ${x+y}")
+    println("x - y = ${x-y}")
+    println("x * y = ${x*y}")
+    println("x / y = ${x/y}")
+    println("x % y = ${x%y}")
+
+    var result = x+y
+
+    result += 2
+println("result 1 = $result")
 
 
-   /*
-
-    val myCharValue = 'D'
-    val myBooleanValuealue = false
-
-    */
-
-    println("myCharvalue = $myCharValue \n and \n myBooleanValuealue is $myBooleanValuealue")
+    result -= 2
+    println("result 2 = $result")
 
 
+    result *= 2
+    println("result 3 = $result")
 
+
+    result /= 2
+    println("result 4 = $result")
+
+
+    result %= 2
+    println("result 5 = $result")
+
+
+    println("( 3 + 3 ) * 4 = ${( 3 + 3 ) * 4 }")
+
+    x = 0
+    println("x = ${x++}")
+    println("x + ${ ++x } ")
+
+    println("x = ${x--}")
+    println("x = ${--x}")
 
 }
