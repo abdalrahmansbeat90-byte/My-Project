7f82d82b2b582eb9326e1c09bedaa7097bc17a4a
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-14 21:34:05 +0300

    project7

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,14 @@
 import kotlin.math.hypot
-fun main() {
+fun main(args: Array<String>) {
 
 val alarm = 7
 
-    val text = when (alarm) {
-     in 1..10 -> {
-         "The Time is in the range 1..10"
+    val text = when {
+        alarm <= 10  -> "The Time is in the range 1..10"
+        alarm == 8 || alarm == 7  -> "The time is $alarm"
+        else  -> "The time is $alarm"
      }
 
-        12,14,7 -> {
-          println("some text")
-            "The time is $alarm"
-        }
+    println(text)
 
-
-
-        else  -> {
-           "The time is $alarm"
-        }
-
-    }
-println(text)
-}
+  }
