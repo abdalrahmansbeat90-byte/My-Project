68779f4753c7387de7d392fa88c9c9f97ff2ba2f
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-14 21:20:08 +0300

    project6

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,26 @@
 import kotlin.math.hypot
 fun main() {
-    val myNumber = 150
 
-    if (myNumber >= 150 ) {
-        println("greater than 150")
+val alarm = 7
+
+    when (alarm) {
+     in 1..10 -> {
+         println("The Time is in the range 1..10")
+     }
+
+        12,14,7 -> {
+            println("The time is $alarm")
+        }
+
+      /*
+        7 -> println("The time is $alarm")
+        14 -> println("The time is $alarm")
+       */
+
+        else  -> {
+            println("The time is $alarm")
+        }
+
     }
-else if (myNumber > 90 ) {
-    println("greater than 90")
-}
-    else{
-        println("All the coditions failed")
-    }
-
-
 
 }
