ae82f77987358f2ee55b8b622ae88647c58ffa5b
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-14 22:43:55 +0300

    projict 7 Edit

--- src/Main.kt
+++ src/Main.kt
@@ -7,7 +7,9 @@ val alarm = 7
         alarm <= 10  -> "The Time is in the range 1..10"
         alarm == 8 || alarm == 7  -> "The time is $alarm"
         else  -> "The time is $alarm"
-     }
+    }
+        println("The time is")
+
 
     println(text)
 
