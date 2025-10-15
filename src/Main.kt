6018fe875217d1fc51be47aad9fdc1cf284c9c55
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-15 17:37:56 +0300

    project 13

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,24 @@
 fun main(args: Array<String>) {
-    val max = getMax(5, 9)
-println(max)
+    val max = getMax(5.5, 9.7)
+    println(max)
 }
 
 fun getMax(a: Int, b: Int): Int {
-    val max = if (a > b) a else b
-    return max
 
+    return if (a > b){
+        return a
+    }
+    else {
+        return b
 
+    }
 }
+
+    fun getMax(a: Double, b: Double): Double {
+        if (a > b) {
+            return a
+        } else {
+            return b
+        }
+    }
 
