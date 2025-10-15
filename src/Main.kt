0fa607d65e7bcfd257b4980b5698ef94a7d21dc8
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-15 21:46:40 +0300

    loop 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,12 @@
 fun main(args: Array<String>) {
-   println(sum(5,6,7,10,5,6,4,5,3,4,5,6,4,4,6,5))
+sum(1,4,6,7,8,9,5,7,6)
 
 }
 
-fun sum(vararg number: Int): Int {
-   var result = 0
-    for (numbers in number) {
-        result += numbers
-    }
-    return result
+fun sum(vararg numbers: Int) {
+numbers.forEach {
+    println(it)
+}
+
+
 }
