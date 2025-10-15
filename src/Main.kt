01d0c4a8143862e92806887e593c07a190d23570
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-15 13:11:48 +0300

    project 9

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,16 @@
-fun main() {
-    val num = 5
-
-
-    if (num > 10) {
-        println("Number is greater than 10")
-    } else if (num == 10) {
-        println("Number is equal to 10")
-    } else {
-        println("Number is less than 10")
-    }
-
-
-    val day = 3
-    when (day) {
-        1 -> println("Monday")
-        2 -> println("Tuesday")
-        3 -> println("Wednesday")
-        else -> println("Other day")
-    }
+fun main(args: Array<String>) {
+    val text : String? = "Name"
+if (text != null) {
+println(text.length)
 }
+    else{
+        print("The variabel is null")
+
+    }
+
+}
+
+
+
+
 
