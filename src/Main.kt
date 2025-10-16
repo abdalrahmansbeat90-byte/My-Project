f83d939a0e3ab9b1d0cf5a1ed453941aa7d9e87c
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-16 18:59:50 +0300

    Array 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,8 @@
 fun main(args: Array<String>) {
 
-    var number = 1
-    val lastNumber = 20
-    var evenNumberCounter=0
+    var names =arrayOf("Abd "," Ahmad "," Azoz ")
+    names[0] = "Abood"
+    println("Farst element :${names [0]}")
 
 
-
-    while (number <= lastNumber) {
-        number++
-
-        if (!isEvenNumber(number)) {
-      continue
-
-              }
-        evenNumberCounter++
-        println(number)
-         }
-    println("Total number of even numbers found =$evenNumberCounter ")
-    }
-
-fun isEvenNumber(number: Int): Boolean {
-    if (number % 2 == 0) {
-        return true
-    }
-   else{
-       return false
-    }
 }
