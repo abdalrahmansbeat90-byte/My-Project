f7960f6bbb6d4c8dda7e8db03387fdde659a304e
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-17 19:16:02 +0300

    ex 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,21 @@
 fun main(args: Array<String>) {
+    val  s = 75
+    val g = if ((s>=90)){
+        "A"
+    }else if  (s >=75){
+        "B"
+    }
+    else if (s >= 60) {
+        "C"
+    }
+    else {
+        "F"
+    }
+    println("Score = ${s.toString()}")
 
-    var names =arrayOf("Abd "," Ahmad "," Azoz ")
-    names[0] = "Abood"
-    println("Farst element :${names [0]}")
+
 
 
 }
+
+
