b25c709cc367fa50ab03e2480b978a1c31f90c18
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-18 08:38:40 +0300

    Max and Min Numbers

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,27 @@
 fun main(args: Array<String>) {
-    val  s = 75
-    val g = if ((s>=90)){
-        "A"
-    }else if  (s >=75){
-        "B"
-    }
-    else if (s >= 60) {
-        "C"
-    }
-    else {
-        "F"
-    }
-    println("Score = ${s.toString()}")
 
-
-
-
+    val max : Int = findMax (arrayOf(4,6,7,4,3,6))
+    val min : Int = findMin (arrayOf(4,7,9,20,7,100))
+    println("max valuo is =$max")
+    println("min valuo is =$min")
 }
+fun findMax(numbers:Array<Int>): Int {
+    var max = numbers[0]
 
+    for (number in numbers) {
+        if (number > max) {
+            max = number
+        }
+    }
+return max
+}
+fun findMin(numbers:Array<Int>): Int {
+    var min = numbers[0]
 
+    for (number in numbers) {
+        if (number < min) {
+            min = number
+        }
+    }
+    return min
+}
