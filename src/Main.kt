2b59d02e5ed84423502b47bbb8ebc98e5a750061
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-18 11:39:15 +0300

    OOP 2

--- src/Main.kt
+++ src/Main.kt
@@ -2,54 +2,43 @@ import kotlin.math.max
 
 
 fun main(args: Array<String>) {
-val car1 = Car()
-    car1.name = "Tesla"
-    car1.model = "S Plaid"
-    car1.Color = "Red"
-    car1.doors = 4
+
+    val car1 = Car("Tesla","S Plaid","Red",4)
 
     println("Name = ${car1.name}")
     println("model = ${car1.model}")
     println("Color = ${car1.Color}")
     println("doors = ${car1.doors}")
-
     println("\n")
 
     car1.move()
     car1.stop()
 
-
-
-val car2 = Car()
-    car2.name = "Ford"
-    car2.model = "Mustang"
-    car2.Color = "Blue"
-    car2.doors = 2
+    val car2 = Car("Ford","Mustang","Blue",2)
 
     println("\n")
-    println("Name = ${car2.name}")
+
+    println("Name  = ${car2.name} ")
     println("model = ${car2.model}")
     println("Color = ${car2.Color}")
     println("doors = ${car2.doors}")
 
+    println("\n")
+
     car2.move()
     car2.stop()
 
+
 }
+class Car (name: String, var model: String, var Color: String, var doors: Int) {
 
+    var name = name.trim()
 
-class Car (){
-    var name = ""
-    var model = ""
-    var Color = ""
-    var doors = 0
-
-
-    fun move(){
-        println("The Car is moving")
+    fun move() {
+        println("The Car $name is moving")
     }
 
-    fun stop (){
-        println("The Car has stopped")
+    fun stop() {
+        println("The Car $name has stopped")
     }
 }
