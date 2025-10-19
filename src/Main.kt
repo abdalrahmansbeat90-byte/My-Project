3c0a01625068e5750c7893bc96a7eab7b28c70ae
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-19 19:35:08 +0300

    lateinit  *

--- src/Main.kt
+++ src/Main.kt
@@ -1,37 +1,11 @@
 fun main(args: Array<String>) {
-    val user = User("Alex")
-    val friend = User(firstName = "John", lastname = "Smith")
-val user2 = User(age = 19, firstName = "Ioana", lastname = "Dobbbi")
+    val user = User("Alex", "Dobbin", 23)
+user.favoriteMove="Interstallar"
 
-user.firstName = "vlad"
-
-
-    println(user.firstName)
-    println(user.lastname)
-    println(user.age)
-
-    println("\n")
-
-    println(friend.firstName)
-    println(friend.lastname)
-    println(friend.age)
+println(user.favoriteMove)
 
 }
+class User(var firstName: String, var lastname: String, var age: Int) {
+ lateinit var favoriteMove: String
 
-class User(firstName: String, var lastname: String="lastName", var age: Int=0) {
-    var firstName = firstName
-        get() {
-            return "FirstName: $field"
-        }
-        set(value) {
-            println("$value was assigned to firstName property")
-            field = value
-        }
-//
-//    fun setFirstName(newValue: String) {
-//        this.firstName = newValue
-//    }
-//fun getFirstName(): String {
-//    return this.firstName
-//}
 }
