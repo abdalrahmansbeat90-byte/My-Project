861d98c6bbb49ae4e7c78d1dca15e1b571f63871
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-20 14:38:11 +0300

    Database

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,17 @@
 fun main(args: Array<String>) {
 
-    val instance = Database.getInstance()
-    val instance2=Database.getInstance()
-    println(instance)
-    println(instance2)
+println(Database)
+println(Database)
+println(Database)
+println(Database)
+println(Database)
+println(Database)
+println(Database)
 }
-
-class Database private constructor() {
-
-    companion object {
-        private var instance: Database? = null
-
-        fun getInstance(): Database? {
-            if (instance == null) {
-                instance = Database()
-            }
-
-            return instance
-        }
+object Database {
+    init {
+        println("Database Created")
     }
+
 }
+
