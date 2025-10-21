59e508d026017d591a53a1187e1ee431c1d11432
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-21 12:25:05 +0300

    SimpleBank

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,49 @@
+package com.example.main
+
+import java.awt.Window
+
 fun main(args: Array<String>) {
-    val listView = ListView(arrayOf("Name 1", "Name 2", "Name 3", "Name 4"))
-    listView.ListViewItem().displayItems(2)
+    val alexAccount = Account("Alex")
+    alexAccount.deposit(1000)
+    alexAccount.withdraw(500)
+    alexAccount.deposit(-20)
+    alexAccount.withdraw(-100)
+
+    val balans = alexAccount.calculateBalance()
+    println("Balance is $balans")
+
 }
 
-class ListView(val items: Array<String>) {
-    inner class ListViewItem {
-        fun displayItems(position: Int) {
-            println(items[position])
+class Account(val accountName: String) {
+    var balance = 0
+    var transactions = mutableListOf<Int>()
+
+    fun deposit(amount: Int) {
+        if (amount > 0) {
+            transactions.add(amount)
+            balance += amount
+            println("$amount deposited. Balance is now ${this.balance}")
+        } else {
+            println("Cannot deposited negative sums")
         }
+    }
+
+    fun withdraw(withdrawal: Int) {
+        if (-withdrawal < 0) {
+            transactions.add(-withdrawal)
+            this.balance += withdrawal
+            println("$withdrawal withdrawal. Balance is now ${this.balance}")
+        } else {
+            println("Cannot withdrawal negative sums")
+        }
+    }
+
+    fun calculateBalance(): Int {
+        this.balance = 0
+
+        for (transaction in transactions) {
+            this.balance += transaction
+        }
+        return this.balance
     }
 }
