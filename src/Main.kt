ae2c059f05a74c8a22be89f35b85c184228250a1
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-22 13:55:40 +0300

    Override

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,34 @@
 fun main(args: Array<String>) {
+    val user1 = User("Alex", "Dobbin", 23)
+    val user2 = User("john", "Dobbin", 23)
 
-}
-
-abstract class Vehicle {
+println(user1.equals(user2))
 
 
-
-   abstract fun move()
-
-   abstract fun stop()
+    println(user1)
+    println(user2)
 
 
 }
 
+class User(var firstName: String, var lastName: String, var age: Int): Any(){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+        if (other is User) {
+            return firstName == other.firstName
+            && this.lastName == other.lastName
+            && this.age == age
+        }
 
-class Car(var name : String = "", var color : String ,val engines: Int ,val doors: Int) : Vehicle(){
-
-    override fun move() {
-
+        return false
+    }
+    override fun hashCode(): Int {
+        return 0
+    }
+    override fun toString(): String {
+        return "User (  firstName = $firstName,  lastName = $lastName,  age = $age  )"
     }
 
-    override fun stop() {
-
-         }
-
-    }
+}
