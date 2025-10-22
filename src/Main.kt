340ffe71e363aecf3576f4782afcff5c1e7c0361
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-22 14:30:13 +0300

    project **

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
     val user1 = User("Alex", "Dobbin", 23)
     val user2 = User("john", "Dobbin", 23)
 
-println(user1.equals(user2))
+    println(user1.equals(user2))
 
 
     println(user1)
@@ -11,24 +11,8 @@ println(user1.equals(user2))
 
 }
 
-class User(var firstName: String, var lastName: String, var age: Int): Any(){
-    override fun equals(other: Any?): Boolean {
-        if (this === other) {
-            return true
-        }
-        if (other is User) {
-            return firstName == other.firstName
-            && this.lastName == other.lastName
-            && this.age == age
-        }
+data class User(var firstName: String, var lastName: String, var age: Int): Any(){
 
-        return false
-    }
-    override fun hashCode(): Int {
-        return 0
-    }
-    override fun toString(): String {
-        return "User (  firstName = $firstName,  lastName = $lastName,  age = $age  )"
-    }
 
 }
+
