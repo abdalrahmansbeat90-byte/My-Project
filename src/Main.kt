1f857a58e92c27e82e5c100271770653e2644a92
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-26 23:09:53 +0300

    Add ti mutableMapOf

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     val users = mutableMapOf<Int, String>(1 to "Maria", 2 to "Alex", 3 to "John")
+   users[5]="vlad"
     users.forEach { t, u ->
         println("$t and $u")
     }
