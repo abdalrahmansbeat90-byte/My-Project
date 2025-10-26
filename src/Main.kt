8bad78115f8a53ecae43c79839d65202e6ed390b
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-26 18:33:21 +0300

    apOf

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,8 @@
 fun main(args: Array<String>) {
-    val User1 = User("Name 1")
-    val User2 = User("Name 2")
-    val User3 = User("Name 3")
-    val User4 = User("Name 4")
-    val User5 = User("Name 5")
-    val User6 = User("Alex")
-    val User7 = User("Alex")
 
+val Users = mapOf<Int, String>(1 to "maria",2 to "Alex",3 to "John" )
 
-
-    val names = mutableSetOf<String>(User1.name, User2.name, User3.name, User4.name, User5.name, User6.name)
-
-
-
-
-    names.forEach { println(it) }
+    println(Users[1])
 
 
 }
-
-class User (val name:String)
