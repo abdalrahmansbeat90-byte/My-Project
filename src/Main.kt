eebfa622d1a550a5598690777190c38edf97af84
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-26 19:22:00 +0300

    mutableMapOf

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,6 @@
 fun main(args: Array<String>) {
-
-val Users = mapOf<Int, String>(1 to "maria",2 to "Alex",3 to "John" )
-
-    println(Users[1])
-
-
+    val users = mutableMapOf<Int, String>(1 to "Maria", 2 to "Alex", 3 to "John")
+    users.forEach { t, u ->
+        println("$t and $u")
+    }
 }
