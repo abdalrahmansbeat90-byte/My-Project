49aef654573ba00552229e4f2843ec2b0f255b37
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-28 20:34:28 +0300

    associateWith

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,15 @@
+import javax.xml.crypto.KeySelector
+import kotlin.coroutines.CoroutineContext
+
 fun main(args: Array<String>) {
 
-    val numbers = setOf(1,2,3,4,5)
-    println(numbers.map { if (it == 3) it * 100 else it * 10  })
-    println(numbers.mapIndexedNotNull { index, value -> if (index == 0) null else index * value })
+    val numbers = listOf("one", "two", "three", "four")
 
+    println(numbers.associateWith { it.length })
 
-    val numbersMap = mapOf("Key 1" to 1, "Key 2" to 2, "Key 3" to 3, "Key 4" to 4)
-    println(numbersMap.mapKeys { it.key.uppercase() })
-    println(numbersMap.mapValues { it.value + it.key.length })
+    println(numbers.associateBy { it.first().uppercase() })
 
+    println(numbers.associateBy(keySelector = { it.first().uppercase() }, valueTransform = { it.length }))
+}
 
-    val colors = listOf("red","brown","grey")
-    val animals = listOf("fox","bear","wolf")
-    println(colors zip animals)
-
-    println(colors.zip(animals){ color, animal -> "The ${animal.replaceFirstChar { it.uppercase() }} is $color" })
-
-
-
-val numberPairs = listOf("one" to 1,"two" to 2,"three" to 3,"four" to 4)
-
-    println(numberPairs)
-    println(numberPairs.unzip())
-
-    }
 
