ff0aa3702ae3200fef9b4ba1c51ecc06cf49a6f3
abdalreahman <abdalrahmansbeat90@gmail.com>
2025-10-29 11:02:32 +0300

    associateBy

--- src/Main.kt
+++ src/Main.kt
@@ -10,6 +10,14 @@ fun main(args: Array<String>) {
     println(numbers.associateBy { it.first().uppercase() })
 
     println(numbers.associateBy(keySelector = { it.first().uppercase() }, valueTransform = { it.length }))
+
+    //flotten
+
+    val numbersSets = listOf(setOf(1,2,3) ,setOf(4,5,6), setOf(7,8,9))
+for (numbers in numbersSets) {
+    for (number in numbers) {
+        println(number)
+    }
+    println("\n")
 }
-
-
+}
